20c314b7d556b81572704d393bc0559c93b5991a
aansadiqul <55220383+aansadiqul@users.noreply.github.com>
2023-01-04 21:46:27 +0600

    Add files via upload
    change some code

--- assignment-part-one.js
+++ assignment-part-one.js
@@ -1,34 +1,67 @@
-const product={};
+const product = {};
+    const category = {};
 
- function getProductDetails(){
-  let product =   JSON.parse(localStorage.getItem('ins-default-attributes'));
-  let {data:{last_visited_product_name, last_visited_product_price, last_visited_product_id}} = product;
-  let name = last_visited_product_name.replace(/%20/g,' ');
-  product.name = name;
-  product.price = last_visited_product_price;
-  product.id = last_visited_product_id;
+    const getProductDetails = async () => {
+      let product = JSON.parse(localStorage.getItem('ins-default-attributes'));
+      const baseUrl = 'https://api-online.myer.com.au/v2/product'
+      const { data: { last_visited_product_id: productId } } = product;
+      const productDetails = await getData(`${baseUrl}/details/bymfp/${productId}`);
+      const [firstProduct] = productDetails;
+      product.name = firstProduct.name;
+      product.price = last_visited_product_price;
+      product.id = firstProduct.id;
+      const productDiscountedPrice = await getData(`${baseUrl}/pricesupplemental?products=${productId}`);
+      const { listPriceFrom: prevPrice, priceFrom: cuurentPrice } = productDiscountedPrice;
+      product.prevPrice = prevPrice;
+      product.cuurentPrice = cuurentPrice;
+      // check for variants
+      if (productDetails.length > 1) {
+        const variants = productDetails.slice(1).map(item => item.id);
+        product.variants = variants;
+      }
+    }
 
- }
+    const submit = (body, url) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error(`Error: ${response.status}`);
+          }
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    }
 
- function getAllAvailableColors(){
- let data = document.querySelector('[data-automation=pdp-colour-container-desktop]');
- let {children} = data;
- let colors  = colors?.children?.map(item=>{
-  let hrefData =item?.attributes?.href?.value?.split('-');
+    const getData = async (url) => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+    const getCategory = async () => {
 
-       return{
-          color: item?.attributes?.value,
-          link:item?.attributes?.href?.value,
-          sku: hrefData[hrefData.length-2]
-       }
-      
- });
+      category = await getData('https://api-online.myer.com.au/v2/category/tree/top');
+     
+    }
 
- product.availableColors= colors;
- }
 
- function getDiscountedPrice(){
-  let discountedPrice = document.querySelector('discounted-price').innerText;
-  product.discountedPrice = discountedPrice;
- }
- // send product object
+    const submitProduct = () => {
+      submit(product, url);
+    }
+
+  
+    const submitCategory = () => {
+      submit(category, url)
+    }
